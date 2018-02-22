7f2cc40c01cf7ee91bb85ffca631403cc99c5c2c
Shunsuke Kirino <shunsuke.kirino@gmail.com>
2018-02-22 17:14:12 +0900

    v0.1.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule BatchedCommunication.MixProject do
   def project() do
     [
       app:               :batched_communication,
-      version:           "0.1.0",
+      version:           "0.1.1",
       elixir:            "~> 1.6",
       build_embedded:    Mix.env() == :prod,
       start_permanent:   Mix.env() == :prod,
