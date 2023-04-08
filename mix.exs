d14b200b505d3b325a006369df3a8e9e74b65127
Shunsuke Kirino <skirino@flywheel.jp>
2023-04-08 22:41:26 +0900

    Update dev/test-only deps

--- mix.exs
+++ mix.exs
@@ -30,9 +30,9 @@ defmodule BatchedCommunication.MixProject do
   defp deps() do
     [
       {:croma      , "~> 0.10"},
-      {:dialyxir   , "~> 0.5" , [only: :dev]},
-      {:ex_doc     , "~> 0.21", [only: :dev]},
-      {:excoveralls, "~> 0.11", [only: :test]},
+      {:dialyxir   , "~> 1.3" , [only: :dev , runtime: false]},
+      {:ex_doc     , "~> 0.29", [only: :dev , runtime: false]},
+      {:excoveralls, "~> 0.16", [only: :test, runtime: false]},
     ]
   end
 
